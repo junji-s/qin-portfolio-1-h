933c37b2e6de0a09a8b63d0afa3e75a419058367
junji-s <40999569+junji-s@users.noreply.github.com>
2022-08-16 18:45:19 +0900

    フッターのコーディング

---
+++ src/components/organisms/layouts/Footer.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <StFooter>
+      <small className="copy">© ️2022 Shimabu IT University</small>
+    </StFooter>
+  );
+};
+
+const StFooter = styled.footer`
+  border-top: 1px solid #e9ecef;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 65px;
+  .copy {
+    color: #868e96;
+    font-size: 10px;
+  }
+`;
