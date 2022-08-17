1d591d9738a94b080f9c41fe9c76e3e26f0cd5a6
junji-s <40999569+junji-s@users.noreply.github.com>
2022-08-17 13:38:07 +0900

    トップページのコンテンツをコーディング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
 import type { NextPage } from "next";
+
 import { IndexFv } from "src/components/organisms/index/IndexFv";
+import { BlogSection } from "src/components/organisms/index/BlogSection";
+import { PortfolioSection } from "src/components/organisms/index/PortfolioSection";
+import { Container, Grid } from "@mantine/core";
+import { GitHubSection } from "src/components/organisms/index/GitHubSection";
+import { TwitterSection } from "src/components/organisms/index/TwitterSection";
 
 const Home: NextPage = () => {
   return (
     <>
       <IndexFv />
+      <BlogSection />
+      <PortfolioSection />
+      <Container>
+        <Grid grow gutter={60}>
+          <Grid.Col span={1}>
+            <GitHubSection />
+          </Grid.Col>
+          <Grid.Col span={1}>
+            <TwitterSection />
+          </Grid.Col>
+        </Grid>
+      </Container>
     </>
   );
 };
