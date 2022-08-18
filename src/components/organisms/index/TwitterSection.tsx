04b2ad05ea18248c81ee89a76469c9159b5c4811
junji-s <40999569+junji-s@users.noreply.github.com>
2022-08-18 10:27:53 +0900

    文言の修正

--- src/components/organisms/index/TwitterSection.tsx
+++ src/components/organisms/index/TwitterSection.tsx
@@ -11,13 +11,15 @@ export const TwitterSection: FC = () => {
   return (
     <StSection>
       <PrimaryTitle order={2}>Twitter</PrimaryTitle>
+
       <ScrollArea type="auto" style={{ height: 700 }}>
         <TwitterContents />
         <TwitterContents />
         <TwitterContents />
       </ScrollArea>
+
       <Box pt={24} className="text-center">
-        <PrimaryBtn href="#">View on GitHub</PrimaryBtn>
+        <PrimaryBtn href="#">View on Twitter</PrimaryBtn>
       </Box>
     </StSection>
   );
