a53768db85ec4c69e8a608a3076e385b16eb36a1
junji-s <40999569+junji-s@users.noreply.github.com>
2022-08-18 20:15:02 +0900

    少し修正

--- src/style/css/layout/StSection.tsx
+++ src/style/css/layout/StSection.tsx
@@ -1,12 +1,13 @@
 import React, { FC, ReactNode } from "react";
+import type { CSSProperties } from "react";
 
 import { Box } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 
-export const StSection: FC<{ children: ReactNode; style?: any }> = ({
-  children,
-  style,
-}) => {
+export const StSection: FC<{
+  children: ReactNode;
+  style?: CSSProperties;
+}> = ({ children, style }) => {
   const largeScreen = useMediaQuery("(min-width: 768px)");
 
   return (
