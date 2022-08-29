0e8eaaa9a496974de03d520a91b6070162e93da4
junji-s <40999569+junji-s@users.noreply.github.com>
2022-08-29 20:53:49 +0900

    日付のカスタマイズする機能を作成

---
+++ src/lib/dayjs.ts
@@ -0,0 +1,9 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const dayjsConversion = (day: string) => {
+  return dayjs.utc(day).tz("Asia/Tokyo").format("YYYY-MM-DD");
+};
