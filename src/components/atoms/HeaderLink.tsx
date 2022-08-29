924caa3247b56894cc4bc319361a7f8ff471650a
junji-s <40999569+junji-s@users.noreply.github.com>
2022-08-29 09:47:48 +0900

    ブログ詳細ページでもヘッダーに下線がつくように修正

--- src/components/atoms/HeaderLink.tsx
+++ src/components/atoms/HeaderLink.tsx
@@ -13,7 +13,9 @@ export const HeaderLink: FC<{ text: string; href: string }> = ({
     <Link href={href}>
       <a
         className={`u-avenir-bold leading-non border-b-2 border-white text-[18px] font-bold  ${
-          router.pathname === "/" + text.toLowerCase() ? "border-black" : ""
+          router.pathname.indexOf(text.toLowerCase()) !== -1
+            ? "border-black"
+            : ""
         }`}
       >
         {text}
